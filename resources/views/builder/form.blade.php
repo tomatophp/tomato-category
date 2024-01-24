023fe16deb7eaad6fb82826f90ff3f3abf663484
Fady Mondy <info@3x1.io>
2024-01-24 15:49:53 +0200

    update translation inputs on builder

--- resources/views/builder/form.blade.php
+++ resources/views/builder/form.blade.php
@@ -1,7 +1,8 @@
 <x-tomato-admin-container :label="isset($item) ? __('Update') . ' ' . $label  : __('Add') . ' ' . $label ">
 <x-splade-form class="grid grid-cols-2 gap-4" method="POST" action="{{route('admin.types.'.$for.'.'.$type.'.store')}}" :default="$item ?? []">
-    <x-splade-input label="{{__('Name [AR]')}}" placeholder="{{__('Name [AR]')}}" name="name.ar" />
-    <x-splade-input label="{{__('Name [EN]')}}" placeholder="{{__('Name [EN]')}}" name="name.en" />
+    <div class="col-span-2">
+        <x-tomato-translation  label="{{__('Name')}}" placeholder="{{__('Name')}}" name="name" />
+    </div>
     <x-splade-input class="col-span-2" label="{{__('Key')}}" placeholder="{{__('Key')}}" name="key" />
     <div class="flex justify-between gap-4 col-span-2">
         <x-tomato-admin-icon class="w-full" label="{{__('Icon')}}" placeholder="{{__('Icon')}}" name="icon" />
